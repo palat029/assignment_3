790f1573f49ed3289e83f7cca09eb6b87815c502
palat029 <TatyanaPalacios@tatyanas-mbp.wireless.newschool.edu>
2015-09-24 13:51:57 -0400

    color

--- challenge_1/sketch.js
+++ challenge_1/sketch.js
@@ -7,12 +7,22 @@ function setup() {
 
 function draw() {
 	// clear the background
-	background(50, 50, 150);
+	background(50, 50, 50);
 
 	// set a fill color
-	fill(255, 255, 255);
+	fill(0, 0, 0);
 
-	// draw the ellipse
-	var diameter = random(100, 200);
-	ellipse(320, 180, diameter, diameter);
+	// set stroke
+	stroke(255, 255, 255);
+
+	// draw the rectangle
+	var rectW = (53);{
+	var rectH = (53);
+
+	for(var y = 0; y < 4; y++)
+	for(var x = 0; x < 10; x++)
+			rect(x * 60 + 28, y * 0 + 220, rectW, rectH );
+		}
+
+
 }
