682c6f0b4a35e339836446ac15a1120780cff7b4
palat029 <TatyanaPalacios@tatyanas-mbp.wireless.newschool.edu>
2015-09-25 15:39:08 -0400

    bouncing

--- challenge_5/sketch.js
+++ challenge_5/sketch.js
@@ -2,17 +2,16 @@ function setup() {
 	// create a place to draw
 	createCanvas(640, 360);
 	noStroke();
-	noLoop();
 }
 
 function draw() {
 	// clear the background
-	background(150, 150, 50);
+	background(0, 0, 0);
 
 	// set a fill color
 	fill(255, 255, 255);
 
 	// draw the ellipse
-	var diameter = random(100, 200);
-	ellipse(320, 180, diameter, diameter);
+	var x = random(-10, 10);
+	ellipse(mouseX + x, mouseY + x, 50, 50);
 }
