8ad3efc5cf4ca53234fc84590ac5b624970d7ce9
palat029 <TatyanaPalacios@Tatyanas-MacBook-Pro.local>
2015-09-25 05:28:34 -0400

    Tiles_1

--- challenge_1/sketch.js
+++ challenge_1/sketch.js
@@ -3,6 +3,7 @@ function setup() {
 	createCanvas(640, 360);
 	noStroke();
 	noLoop();
+
 }
 
 function draw() {
@@ -22,7 +23,31 @@ function draw() {
 	for(var y = 0; y < 4; y++)
 	for(var x = 0; x < 10; x++)
 			rect(x * 60 + 28, y * 0 + 220, rectW, rectH );
-		}
+
+			// draw rectangles
+	 	 fill(0, 0, 0);
+	 	 rect(28, 220, rectW, rectH);
+		 fill(30, 10, 12);
+	 	 rect(88, 220, rectW, rectH);
+		 fill(51, 16, 16);
+	 	 rect(148, 220, rectW, rectH);
+		 fill(81, 16, 16);
+		 rect(208, 220, rectW, rectH);
+		 fill(111, 15, 16);
+		 rect(268, 220, rectW, rectH);
+		 fill(141, 23, 27);
+		 rect(328, 220, rectW, rectH);
+		 fill(169, 29, 34);
+		 rect(388, 220, rectW, rectH);
+		 fill(198, 32, 38);
+		 rect(448, 220, rectW, rectH);
+		 fill(227, 30, 38);
+		 rect(508, 220, rectW, rectH);
+		 fill(237, 30, 35);
+		 rect(568, 220, rectW, rectH);
+
+}
+
 
 
 }
