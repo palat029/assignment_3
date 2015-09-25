e3eda37a1da179d1cceb85bc53b03e4748a96a84
palat029 <TatyanaPalacios@tatyanas-mbp.wireless.newschool.edu>
2015-09-25 14:39:54 -0400

    Blue Target

--- challenge_2/sketch.js
+++ challenge_2/sketch.js
@@ -1,3 +1,6 @@
+var targetSize = 20;
+var targetSizeN = 40;
+
 function setup() {
 	// create a place to draw
 	createCanvas(640, 360);
@@ -11,18 +14,6 @@ function draw() {
 	// draw the ellipse
 	stroke(255, 255, 255);
 	noFill();
-	var diameter = (53);
-	var diameterT = (53);
-		for (var y = 0; y < 4; y++)
-			if ( y === 2){
-					stroke(255, 255, 255);
-					noFill();
-			}	else {
-					stroke(0, 0, 0);
-					noFill();
-			}
-			for (var x = 0; x< 6; x++){
-		 		ellipse(x * 70, y * 10 + 220, diameter, diameterT);
-}
+
 
 }
--- challenge_3/sketch.js
+++ challenge_3/sketch.js
@@ -2,17 +2,38 @@ function setup() {
 	// create a place to draw
 	createCanvas(640, 360);
 	noStroke();
-	noLoop();
+
 }
 
 function draw() {
 	// clear the background
-	background(150, 50, 150);
+	background(150, 26, 30);
 
 	// set a fill color
-	fill(255, 255, 255);
-
+	fill(150, 26, 30);
+	stroke(255, 255, 255);
 	// draw the ellipse
-	var diameter = random(100, 200);
-	ellipse(320, 180, diameter, diameter);
+	line(mouseX, 0, mouseX, 640);
+	line(640, mouseY, 0, mouseY);
+	ellipse(mouseX, mouseY, 30, 30);
+
+	if(mouseIsPressed){
+		  background(50, 50, 100);
+			fill(50, 50, 100);
+			stroke(255, 255, 255);
+			line(mouseX, 0, mouseX, 640);
+			line(640, mouseY, 0, mouseY);
+			ellipse(mouseX,mouseY,30, 30);
+	} else {
+			background(150, 26, 30);
+			fill(150, 26, 30);
+			stroke(255, 255, 255);
+			line(mouseX, 0, mouseX, 640);
+			line(640, mouseY, 0, mouseY);
+			ellipse(mouseX, mouseY, 30, 30);
+
+	}
+
+
+
 }
